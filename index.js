8938315378aa62696237896b89468182388f2bca
Charles Chavis <chavisca@gmail.com>
2023-07-31 23:56:36 -0400

    inquirer added, inquire JS barebone added

---
+++ index.js
@@ -0,0 +1,28 @@
+// TODO:  Prompt for information about application repo
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+inquirer
+  .Prompt([
+    {
+
+    }
+  ])
+  .then((response) => {
+    
+  });
+// Markdown readme should be generated with title of project and sections for Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
+
+// TODO:  Project title should be displayed as title of the readme
+
+// TODO:  Description, Installation Instructions, Usage Info, Contribution Guidelines, and Test Instructions need to populate to appropriate sections
+
+// TODO:  List of options should be presented to choose license, then badge for license is added near the top of the README and a notice is added that explains the license covering
+
+// TODO:  When prompted for GitHub u/n, add the u/n under Questions with a link to the github profile
+
+// TODO:  When prompted for email address, add the email under Questions with instructions on how to reach
+
+// TODO:  Links in the Table of Contents should go to the corresponding section of the README.
+
